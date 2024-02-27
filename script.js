d2dc3cf6553adf49d5bb3dc7be0aad49a7bd92bb
Koty Stannard <k.stannard@live.com>
2024-02-26 18:30:58 -0600

    Add round logic to game flow

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ const gameBoard = (() => {
     const getBoard = () => board
 
     const addMarker = (marker, row, col) => {
-        board[row][col] = marker
+        if(board[row][col] === 0) {
+            board[row][col] = marker
+        } else {
+            console.log("Invalid cell")
+        }
+    }
+
+    const cellAvailable = (row, col) => {
+        return board[row][col] === 0 ? true : false
     }
 
     const printBoard = () => console.log(board.map(row => row.join(' ')).join('\n'))
@@ -15,6 +23,7 @@ const gameBoard = (() => {
     return {
         getBoard,
         addMarker,
+        cellAvailable,
         printBoard
     }
 })()
@@ -25,3 +34,44 @@ function player(name, marker) {
         marker: marker 
     }
 }
+
+const game = (() => {
+    const players = [
+        player("Player One", 1),
+        player("Player Two", 2)
+    ]
+
+    let playerTurn = players[0]
+
+    const switchTurns = () => {
+        playerTurn = playerTurn === players[0] ? players[1] : players[0]
+    }
+
+    const getActivePlayer = () => playerTurn
+
+    const playRound = (row, col) => {
+        console.log(`${getActivePlayer().name}'s turn - placing marker at [${col}][${row}]`)
+
+        if(gameBoard.cellAvailable(row, col)) {
+            gameBoard.addMarker(getActivePlayer().marker, row, col)
+            gameBoard.printBoard()
+            switchTurns()
+            console.log(`Switching turns - ${getActivePlayer().name}'s turn`)
+        } else {
+            console.log("Cell filled - try again")
+        }
+    }
+
+    return {
+        switchTurns,
+        getActivePlayer,
+        playRound
+    }
+})()
+
+game.playRound(0, 0)
+game.playRound(0, 0)
+game.playRound(1, 0)
+game.playRound(0, 1)
+game.playRound(2, 1)
+game.playRound(0, 2)
